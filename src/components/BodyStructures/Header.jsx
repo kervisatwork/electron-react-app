275cc8bbfbe4e9d2126736c5a6c0057e0630cbaf
KervisatWork <kervijhonata@gmail.com>
2026-06-15 17:58:41 -0300

    style|refactor: adicionando react-router-dom e realocando arquivos da estrutura principal

--- src/components/BodyStructures/Header.jsx
+++ src/components/BodyStructures/Header.jsx
@@ -1,21 +1,56 @@
 import styles from "./Header.module.css"
 import logoImg from "../../assets/images/herbopediaLogo.svg"
+import PropTypes from "prop-types";
 
 function Header({userName}) {
 
     return (
         <header className={styles.globalHeader}>
+            
             <div className={styles.logo}>
                 <img src={logoImg} alt="logo" />
                 <h1>Herbapedia</h1>
             </div>
             <div className={styles.menuAction}>
                 <a href="#/login" className={styles.login}>Login</a>
-                <button>
+                <button alt="open Menu" onClick={() => {
+                    const menu = document.querySelector(`.${styles.navMenu}`)
+                    menu.style.display = "block"
+                }}>
                     <div className={styles.bar}></div>
                 </button>
             </div>
+
+            <nav className={styles.navMenu}>
+                <div className={styles.menuAction}>
+                    <h2>Menu</h2>
+                    <button alt="close Menu" onClick={() => {
+                        const menu = document.querySelector(`.${styles.navMenu}`)
+                        menu.style.display = "none"
+                    }}>
+                        X
+                    </button>
+                </div>
+                <div className={styles.menuBody}>
+                <ul>
+                    <li><a href="#/login">Login</a></li>
+                    <li><a href="#/home">Home</a></li>
+                    <li><a href="#/about">Sobre</a></li>
+                    <li><a href="#/contact">Contato</a></li>
+                    <li><a href="#/themes">Temas</a></li>
+                </ul>
+                </div>
+            </nav>
+
         </header>
+    )
+}
+
+function MenuList(list) {
+    return (
+        <ul>
+            {{}}
+        </ul>
     )
 }
 
---
+++ src/pages/ModelTest.jsx
@@ -0,0 +1,12 @@
+class ModelTest extends Component {
+    render() {
+        return (
+            <div>
+                Olá, Kervi!
+            </div>
+        )
+    }
+}
+
+
+export default ModelTest;
